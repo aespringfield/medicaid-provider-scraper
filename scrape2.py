0192a080130e3ecc1315fa99471f84145110d162
Anna Springfield <anna.e.springfield@gmail.com>
2018-03-24 12:11:23 -0500

    start text file reading functions

--- scrape2.py
+++ scrape2.py
@@ -3,34 +3,53 @@ import sys
 import json
 import urllib
 import base64
-import requests
+# import requests
 
-def search_twitter(path):
+def read_providers_file(path):
     # read file..
-
+    providers_file = open(path)
+    return providers_file
 
 def parse_file(file):
     counter = 0
-    dentists = []
+    providers = []
     data = {}
-    for each line in file:
+    for line in file:
+        if line == '\n':
+            providers.append(data)
+            data = {}
+            counter = 0
+        else:
+            attribute, value = parse_line(line, counter)
+            data[attribute] = value
+            counter += 1
+    print providers
+
+def parse_line(line, counter):
+    if counter == 0:
+        attribute = 'name'
+        value = line.strip()
+
+    return [attribute, value]
+
+#         loop over line and regex for data...
 
 
-        loop over line and regex for data...
+#         data['name'] = title
+#         data['address1'] = blah1
+#         data['address2'] = blah2
+
+#     dentists.append(data)
+
+#     return dentists
 
 
-        data['name'] = title
-        data['address1'] = blah1
-        data['address2'] = blah2
+# def main():
+#     file = read_txt_file('dental_clinic')
+#     parse_file(file)
 
-    dentists.append(data)
+# if __name__ == '__main__':
+#     main()
 
-    return dentists
-
-
-def main():
-    file = read_txt_file('dental_clinic')
-    parse_file(file)
-
-if __name__ == '__main__':
-    main()
+my_file = read_providers_file('./dental_clinic_mn.txt')
+parse_file(my_file)
