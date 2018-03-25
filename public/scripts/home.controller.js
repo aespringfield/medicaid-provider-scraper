651c1fce5ef58d6330ab4ad5ac6b8a441a974843
joshnothum <joshnothum@gmail.com>
2018-03-24 21:51:09 -0500

    input dropdown is bound to HC array

--- public/scripts/home.controller.js
+++ public/scripts/home.controller.js
@@ -1,5 +1,10 @@
 myApp.controller('HomeController', function(NgMap){
-    console.log('HomeController created')
-
+    console.log('HomeController created');
+    let home = this;
     
-})
+    home.selectedType = [];
+    
+
+    home.types =[]
+    
+});
